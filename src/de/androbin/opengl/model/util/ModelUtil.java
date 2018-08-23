948e06dab33a997f438b6c747c0ba0061e94d891
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2018-08-23 04:31:21 +0200

    Adapt IO changes

--- src/de/androbin/opengl/model/util/ModelUtil.java
+++ src/de/androbin/opengl/model/util/ModelUtil.java
@@ -3,13 +3,19 @@ package de.androbin.opengl.model.util;
 import de.androbin.io.util.*;
 import de.androbin.opengl.model.*;
 import java.io.*;
+import java.nio.file.*;
 
 public final class ModelUtil {
   private ModelUtil() {
   }
   
-  public static Model loadModel( final File file ) {
-    return FileReaderUtil.readFile( file, reader -> loadModel( reader ) );
+  public static Model loadModel( final Path file ) {
+    try {
+      return FileReaderUtil.readFile( file, reader -> loadModel( reader ) );
+    } catch ( final IOException e ) {
+      e.printStackTrace();
+      return null;
+    }
   }
   
   public static Model loadModel( final BufferedReader reader ) {
